27763cd1e73d27d75fc0fb0b05e34c47b00362dc
Alexandru88 <andrasescu_alexandru@yahoo.com>
2014-07-24 19:03:47 +0300

    added ActionBarSherlock

--- SocialIntegration/Dashboard.cs
+++ SocialIntegration/Dashboard.cs
@@ -11,19 +11,38 @@ using Android.Views;
 using Android.Widget;
 using Android.Graphics;
 
+using Xamarin.ActionbarSherlockBinding.App;
+using Xamarin.ActionbarSherlockBinding.Views;
+using Xamarin.ActionbarSherlockBinding.Widget;
+
+
+
 namespace SocialIntegration
 {
-    [Activity(Label = "Dashboard", MainLauncher = false, Icon = "@drawable/icon")]
-    public class Dashboard : Activity
+    [Activity(Label = "Dashboard", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Dashboard : SherlockActivity
     {
+        private static Handler handler = new Handler();
+        private bool useLogo = false;
+        private bool showHomeUp = false;
+
         protected override void OnCreate(Bundle bundle)
         {
             try
             {
+                SetTheme(Resource.Style.Theme_Sherlock_Light);
                 base.OnCreate(bundle);
-                RequestWindowFeature(WindowFeatures.NoTitle);
-
+                //RequestWindowFeature(WindowFeatures.NoTitle);
                 SetContentView(Resource.Layout.Dashboard);
+                var ab = base.SupportActionBar;
+                
+                //set defaults for logo & home up
+                ab.SetDisplayHomeAsUpEnabled(showHomeUp);
+                ab.SetDisplayUseLogoEnabled(useLogo);
+
+                ////set up list nav
+                ab.SetListNavigationCallbacks(ArrayAdapter.CreateFromResource(this, Resource.Array.sections, Resource.Drawable.abs__ic_menu_moreoverflow_normal_holo_dark), null);
+
                 Button btn_CreateWorkout = FindViewById<Button>(Resource.Id.CreateWorkout);
                 Button btn_Stats = FindViewById<Button>(Resource.Id.Stats);
 
@@ -45,6 +64,23 @@ namespace SocialIntegration
                 ex.ToString();
                 throw;
             }
+        }
+
+
+        public override bool OnCreateOptionsMenu(Xamarin.ActionbarSherlockBinding.Views.IMenu menu)
+        {
+            try
+            {
+                //MenuInflater.Inflate(Resource.Menu.myMenu, menu);
+                //SupportMenuInflater.Inflate(Resource.Menu.main_menu, menu);
+                SupportMenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                throw;
+            }
+            return base.OnCreateOptionsMenu(menu);
         }
     }
 }
